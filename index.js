bd33a34f306f5926565066e1350c919614592fbf
sergiocardenasg <sergiocardenas@utexas.edu>
2021-02-13 21:17:29 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData (name, email) {
+    
+    let formData = {
+        name,
+        email
+      };
+    
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(response => response.json())
+        .then(object => document.body.innerHTML = object['id'])
+        .catch(error => document.body.innerHTML = error.message);
+};
